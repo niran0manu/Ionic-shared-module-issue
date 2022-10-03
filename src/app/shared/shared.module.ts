9048159acf6582009d2292768b5eeb8cf8ad63d7
niran0manu <niran0manu@gmail.com>
2022-10-03 11:58:12 +0300

    Swiper fixed

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TestmoduleComponent } from './testmodule/testmodule.component';
 import { IonicModule } from '@ionic/angular';
+import { SwiperOptions } from 'swiper';
+import { SwiperComponent, SwiperModule } from 'swiper/angular';
+import SwiperCore, {  Pagination,Navigation } from 'swiper';
+
+
+
 
 
 
 
 @NgModule({
-  declarations: [TestmoduleComponent],
+  declarations: [TestmoduleComponent, ],
   imports: [
     CommonModule,
     IonicModule,
+    SwiperModule,
+
 
 
   ],
